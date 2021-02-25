d6063bb8ce214f0f757d0861a2b1da618bb7b8aa
alexcdever <alexcdever@gmail.com>
2021-02-25 23:07:13 +0800

    feat(command): collect OS info

--- cmd/collect.go
+++ cmd/collect.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/host"
+	"github.com/spf13/cobra"
+)
+
+var info, _ = host.Info()
+var collectCmd = &cobra.Command{
+	Use:   "collect",
+	Short: "collect the OS info",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("OS: %v", info.OS)
+		fmt.Printf("platform: %v", info.Platform)
+		fmt.Printf("family: %v", info.PlatformFamily)
+		fmt.Printf("version: %v", info.PlatformVersion)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(collectCmd)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/alexcdever/init-env-cmd-program
 go 1.16
 
 require (
+	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/shirou/gopsutil v3.21.1+incompatible
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
+	golang.org/x/tools v0.0.0-20191112195655-aa38f8e97acc
 )
